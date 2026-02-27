3533340dab07f86159ac0741cc6def0c301f65f7
Graheet <phartyalgraheet23@gmail.com>
2026-02-27 12:51:21 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,10 @@ const io = new Server(httpServer, {
   },
 });
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3080' }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
